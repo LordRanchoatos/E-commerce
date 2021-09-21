dfa96cbd3568d394bddc1074aa5a924ca41e2faa
unknown <atolagbedt@gmail.com>
2021-09-21 18:11:32 +0100

    updated the memnu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import Homepage from './components/homepage/homepage.component';
+
+const page1 = () => {
+  return ( 
+    <div>
+      <h1>page 1 of the app</h1>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+      <Route  path='/' component={Homepage}/>
+      <Route  path='/1' component={page1}/>
+      </Switch>
+      
     </div>
   );
 }
